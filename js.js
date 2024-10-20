60c5a0d25068814aa4321bbc71191bce22c7793f
Ramiro <ramiddelgado@gmail.com>
2024-10-20 04:17:19 -0300

    primer commit: v1.0

---
+++ js.js
@@ -0,0 +1,8 @@
+// Navigation
+function toggleMenu() {
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const navigation = document.querySelector('.navigation');
+    toggleMenu.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+// End Navigation
