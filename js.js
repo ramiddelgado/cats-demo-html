e45d77c2657a3b3027074b4ffc4cfd98aadb2c26
Ramiro Delgado <ramiddelgado@gmail.com>
2024-11-05 18:47:57 -0300

    optimizacion carga imagenes - jpgtowebp

--- js.js
+++ js.js
@@ -8,16 +8,16 @@ document.addEventListener('DOMContentLoaded', function() {
         initGallery();
     }
 
-// document.addEventListener('contextmenu', function(e) {
-//     e.preventDefault();
-// });
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
 
 
-// document.addEventListener('keydown', function(e) {
-//     if (e.key === 'F12') {
-//         e.preventDefault();
-//     }
-// });
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'F12') {
+        e.preventDefault();
+    }
+});
 
     
 
