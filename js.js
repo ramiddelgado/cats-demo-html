eaa7d890c4570214636ae58165d05c948fe13d2c
Ramiro Delgado <ramiddelgado@gmail.com>
2024-11-05 21:02:10 -0300

    widget fix

--- js.js
+++ js.js
@@ -9,6 +9,8 @@ document.addEventListener('DOMContentLoaded', function() {
     if (document.getElementById('gallery')) {
         initGallery();
     }
+
+    initWidget();
     
 
     
@@ -229,17 +231,19 @@ function initTestimonials(){
     
     // !FIN DE GALERIA.HTML
 
-    // Start of OpenWidget (www.openwidget.com) code
-    window.__ow = window.__ow || {};
-    window.__ow.organizationId = "7eeaa7d8-62ab-473d-b90a-c06037687389";
-    window.__ow.integration_name = "manual_settings";
-    window.__ow.product_name = "openwidget";   
-    (function(n,t,c){
-        function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}
-        var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[OpenWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.openwidget.com/openwidget.js",t.head.appendChild(n)}};
-        !n.__ow.asyncInit&&e.init(),n.OpenWidget=n.OpenWidget||e
-    }(window,document,[].slice));
-    // End of OpenWidget code
+function initWidget(){
+        // Start of OpenWidget (www.openwidget.com) code
+        window.__ow = window.__ow || {};
+        window.__ow.organizationId = "7eeaa7d8-62ab-473d-b90a-c06037687389";
+        window.__ow.integration_name = "manual_settings";
+        window.__ow.product_name = "openwidget";
+        (function(n,t,c){
+            function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}
+            var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[OpenWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.openwidget.com/openwidget.js",t.head.appendChild(n)}};
+            !n.__ow.asyncInit&&e.init(),n.OpenWidget=n.OpenWidget||e
+        }(window,document,[].slice));
+        // End of OpenWidget code
+}
 
 
 
