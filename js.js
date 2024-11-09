38f27995c6d8c022c8f2f9b6be4f09448a0923a5
Ramiro Delgado <ramiddelgado@gmail.com>
2024-11-09 12:38:41 -0300

    titulos size fixes

--- js.js
+++ js.js
@@ -15,16 +15,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
     
 
-document.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-});
+// document.addEventListener('contextmenu', function(e) {
+//     e.preventDefault();
+// });
 
 
-document.addEventListener('keydown', function(e) {
-    if (e.key === 'F12') {
-        e.preventDefault();
-    }
-});
+// document.addEventListener('keydown', function(e) {
+//     if (e.key === 'F12') {
+//         e.preventDefault();
+//     }
+// });
 
     
 
